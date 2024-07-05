4acf9b58e23036fe453e1552af20346125b513a8
Mekha <mekhalnair@gmail.com>
2024-07-06 01:13:10 +0530

    addding question page

--- server/tmp_code.py
+++ server/tmp_code.py
@@ -1,16 +1,3 @@
-def maxArea(height):
-    left = 0
-    right = len(height) - 1
-    maxArea = 0
-
-    while left < right:
-        currentArea = min(height[left], height[right]) * (right - left+1)
-        maxArea = max(maxArea, currentArea)
-
-        right -= 1
-
-    return maxArea
-
-height = [4, 3, 2, 1, 4]
-print(maxArea(height))
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+          
